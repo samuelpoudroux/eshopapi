84fb1b05703dc4793aefd6782ba42837510ae532
samuelpoudroux <samuel.poudroux@alten.com>
2020-10-16 16:24:56 +0200

    correction connection db

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    insecureAuth : true,
     database: process.env.DATABASE
   });
 
