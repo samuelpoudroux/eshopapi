50b67f9702b94c9af03cc24957d3170adc89130a
samuelpoudroux <samuel.poudroux@alten.com>
2020-10-17 14:38:19 +0200

    correction localstorage

--- repository/globalSearch.js
+++ repository/globalSearch.js
@@ -3,13 +3,13 @@ const makeDb = require("./makeDb");
 const buildGlobalSearchResult = async () => {
   const db = await makeDb()
   const listTableQuery = `SHOW TABLES FROM ${process.env.DATABASE}`
-  console.log('tata',listTableQuery);
   const collectionsListArray = await db.query(listTableQuery)
+  console.log('tata',collectionsListArray);
   // nous transformons le tableau ci dessous en tableau de promesse par flatmap
   const promises = collectionsListArray.flatMap(async (collection) => {
     const {Tables_in_eshop} = collection
     if (Tables_in_eshop !== 'users') {
-      const query = `SELECT * FROM ${Tables_in_eshop}`
+      const query = `SELECT * FROM '${Tables_in_eshop}'`
       return [
         Tables_in_eshop,
         await  db.query(query)     
