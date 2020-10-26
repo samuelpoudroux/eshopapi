581f5aef05789939dfdda83bb35861d512ba4f88
samuelpoudroux <samuel.poudroux@alten.com>
2020-10-26 19:19:56 +0100

    improvement socket

---
+++ repository/chatManagement.js
@@ -0,0 +1,119 @@
+const socketMessageManagement = (data, messages) => {
+  switch (true) {
+    case data.message === "oui" &&
+      data.questionOrigin === `Avez vous déjà un compte chez nous ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Que souhaitez vous faire ?`,
+        type: "choice",
+        choice: [
+          "Finaliser ma commande",
+          "Gérer mes favoris",
+          "Gérer mon panier",
+          "Modifier mes informations",
+        ],
+      });
+      console.log("malada");
+      return messages;
+      break;
+    case data.message === "non" &&
+      data.questionOrigin === `Avez vous déjà un compte chez nous ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Souhaitez vous vous inscrire ?`,
+        type: "choice",
+        choice: ["oui", "non"],
+      });
+      return messages;
+      break;
+    case data.message === "non" &&
+      data.questionOrigin === `Vous voulez découvrir nos produits ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Souhaitez vous nous contacter ?`,
+        type: "choice",
+        choice: ["oui", "non"],
+      });
+      return messages;
+      break;
+    case data.message === "oui" &&
+      data.questionOrigin === `Souhaitez vous vous inscrire ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Nous vous dirigons vers la page de création`,
+      });
+      return messages;
+      break;
+    case data.message === "non" &&
+      data.questionOrigin === `Souhaitez vous vous inscrire ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Nous vous laissons donc découvrir notre site tranquillement, vous pouvez trouver nos coordonnées via le formulaire de contact. à bientôt spweb`,
+      });
+      return messages;
+      break;
+    case data.message === "oui" &&
+      data.questionOrigin === `Souhaitez vous nous contacter ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Nous vous redirigeons vers notre page contact`,
+      });
+      return messages;
+      break;
+    case data.message === "non" &&
+      data.questionOrigin === `Souhaitez vous nous contacter ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Nous vous laissons donc découvrir notre site, vous pouvez trouver nos coordonnées via le formulaire de contact. à bientôt spweb`,
+      });
+      return messages;
+      break;
+    case data.message === "Gérer mon panier" &&
+      data.questionOrigin === `Que souhaitez vous faire ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Voici votre panier`,
+      });
+      return messages;
+      break;
+    case data.message === "Gérer mes favoris" &&
+      data.questionOrigin === `Que souhaitez vous faire ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Voici vos favoris`,
+      });
+      return messages;
+      break;
+    case data.message === "Finaliser ma commande" &&
+      data.questionOrigin === `Que souhaitez vous faire ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Nous vous dirigeons vers la page de paiement`,
+      });
+      return messages;
+      break;
+    case data.message === "Modifier mes informations" &&
+      data.questionOrigin === `Que souhaitez vous faire ?`:
+      messages.push({
+        id: 0,
+        firstName: "virtuel",
+        message: `Nous vous dirigeons vers la page de gestion du compte`,
+      });
+      return messages;
+      break;
+    default:
+      return messages;
+  }
+};
+
+module.exports = { socketMessageManagement };
--- socket.js
+++ socket.js
@@ -1,3 +1,5 @@
+const { socketMessageManagement } = require("./repository/chatManagement");
+
 module.exports = (socket, io) => {
   let messages = [];
   messages.push(
@@ -18,88 +20,7 @@ module.exports = (socket, io) => {
 
   socket.on("newMessage", (data) => {
     messages.push(data);
-    if (
-      data.message === "oui" &&
-      data.questionOrigin === `Avez vous déjà un compte chez nous ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Vous voulez découvrir nos produits ?`,
-        type: "choice",
-        choice: ["oui", "non"],
-      });
-    } else if (
-      data.message === "non" &&
-      data.questionOrigin === `Avez vous déjà un compte chez nous ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Souhaitez vous vous inscrire ?`,
-        type: "choice",
-        choice: ["oui", "non"],
-      });
-    } else if (
-      data.message === "oui" &&
-      data.questionOrigin === `Vous voulez découvrir nos produits ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Nous vous dirigeons vers nos produits`,
-        redirect: "products",
-      });
-    } else if (
-      data.message === "non" &&
-      data.questionOrigin === `Vous voulez découvrir nos produits ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Souhaitez vous nous contacter ?`,
-        type: "choice",
-        choice: ["oui", "non"],
-      });
-    } else if (
-      data.message === "oui" &&
-      data.questionOrigin === `Souhaitez vous vous inscrire ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Nous vous dirigons vers la page associée`,
-        redirect: "register",
-      });
-    } else if (
-      data.message === "non" &&
-      data.questionOrigin === `Souhaitez vous vous inscrire ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Nous vous laissons donc découvrir notre site tranquillement, vous pouvez trouver nos coordonnées via le formulaire de contact. à bientôt spweb`,
-      });
-    } else if (
-      data.message === "oui" &&
-      data.questionOrigin === `Souhaitez vous nous contacter ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Nous vous redirigeons vers notre page contact`,
-        redirect: "contact",
-      });
-    } else if (
-      data.message === "non" &&
-      data.questionOrigin === `Souhaitez vous nous contacter ?`
-    ) {
-      messages.push({
-        id: 0,
-        firstName: "virtuel",
-        message: `Nous vous laissons donc découvrir notre site tranquillement, vous pouvez trouver nos coordonnées via le formulaire de contact. à bientôt spweb`,
-      });
-    }
+    socketMessageManagement(data, messages);
     io.to(socket.id).emit("newMessage", messages);
   });
   //Disconnect
